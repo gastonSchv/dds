edbc889287194af58242a9fa855011b463ecf2cb
Gaston Schvarchman <janeleker@gmail.com>
2020-04-25 11:17:57 -0300

    Cracion de archivo para borrar luego

---
+++ Parser estado de sincronizacion de calipso no modificado de cuenta mari (236102).js
@@ -0,0 +1,19 @@
+function (salesOrder, parse) {
+    if (salesOrder.deliveryMethod == "ToBeConfirmed") {
+            salesOrder.contact.location = {}
+            salesOrder.contact.location.streetName = "Mosconi";
+            salesOrder.contact.location.streetNumber = "553";
+            salesOrder.contact.location.zipCode = "1643";
+        }
+    return parse(salesOrder).tap(function (venta) {
+        if (salesOrder.contact.billingInfo.docType == "DNI"){
+            venta.cliente.tipoiva = "F";
+        } else if (salesOrder.contact.billingInfo.taxPayerType == "Monotributo") {
+            venta.cliente.tipoiva = "M";
+        } else if (salesOrder.contact.billingInfo.taxPayerType == "Exento") {
+            venta.cliente.tipoiva = "X";
+        } else {
+            venta.cliente.tipoiva = "C";
+        }
+    })
+}
